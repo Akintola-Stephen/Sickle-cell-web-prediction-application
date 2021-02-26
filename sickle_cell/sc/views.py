a34a3b4b17002f5469a2c7c342cba453f2002582
AKINTOLA STEPHEN IYANU <sakintola351@gmail.com>
2021-02-26 09:22:23 +0100

    Updates on the html files

--- sickle_cell/sc/views.py
+++ sickle_cell/sc/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request, template_name="landing-page/index.html"):
-    return render(request, template_name )
+    return render(request, template_name)
