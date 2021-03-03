48f6e0863acc31e4b57afc3cdb64b85f244feef1
AKINTOLA STEPHEN IYANU <40688762+Akintola-Stephen@users.noreply.github.com>
2021-03-03 21:57:24 +0100

    Update views.py
    Update

--- sickle_cell/sc/views.py
+++ sickle_cell/sc/views.py
@@ -7,6 +7,7 @@ def index(request, template_name="landing-page/index.html"):
     return render(request, template_name)
 
 
+# I'll bring in my ML model to this functions so as to automate predictions
 def question_prediction(request, template_name="diagnosis-questions/questions.html"):
     return render(request, template_name)
 
